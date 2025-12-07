ac4d3f7740fa8604f1fbfddf6d0832bccade73f0
Oscar Fernandez <oscar020606@gmail.com>
2025-12-07 13:53:46 -0400

    feat(c++)The calculate function was completed and the optimization function called was added, global variables were added.

--- code.cpp
+++ code.cpp
@@ -6,20 +6,26 @@
 
 using namespace std;
 
+int mejor_suma = -1;
+int mejor_cantidad = 9999;
+int mejor_oscuridad = 9999;
+bool seleccion_actual[32];
+bool mejor_seleccion[32];
+
 struct Circulo{
     int color = 0;
 
     //(Para calcular centro y diametro)
-    int min_fila = 999;
+    int min_fila = 9999;
     int max_fila = -1;
-    int min_col = 999;
+    int min_col = 9999;
     int max_col = -1;
 
     //Variables calculadas
     int centro_fila = 0;
     int centro_col = 0;
     int radio = 0;
-    int diamtro = 0;
+    int diametro = 0;
     int circu = 0;
     double pi = 0.0;
     int area_normalizada = 0;
@@ -77,7 +83,7 @@ void analizar_circulos(file_pgm &img, Circulo lista[]){
 
             if(index == -1){
                 index = n_encontrados;
-                n_encontrados ++;
+                n_encontrados++;
 
                 lista[index].color = pixel;
                 lista[index].min_fila = i;
@@ -95,21 +101,101 @@ void analizar_circulos(file_pgm &img, Circulo lista[]){
     }
 }
 
-void calcular(file_pgm &img, Circulo lista[], int M, int T){
+void calcular(file_pgm &img, Circulo lista[], int M){
 
-    //Ordenar colores
+    //1.Ordenar
     for(int i = 0; i < M - 1; i++){
         for(int j = 0; j < M - i - 1; j++){
             if(lista[j].color > lista[j+1].color){
                 Circulo aux = lista[j];
-                lista[j] = lista[j+1];
+                lista[j] = lista [j+1];
                 lista[j+1] = aux;
             }
         }
     }
 
-    //Calcular
-    double suma_pi = 0.0;
+    double sum_pi = 0.0;
+
+    //2. Calcular Propiedades
+    for(int k = 0; k < M; k++){
+        lista[k].diametro = (lista[k].max_col - lista[k].min_col) + 1;
+        lista[k].radio = (lista[k].diametro + 1) / 2;
+        lista[k].centro_fila = (lista[k].min_fila + lista[k].max_fila) / 2;
+        lista[k].centro_col = (lista[k].min_col + lista[k].max_col) / 2;
+
+        int borde = 0;
+
+        for(int i = lista[k].min_fila; i <= lista[k].max_fila; i++){
+            for(int j = lista[k].min_col; j <= lista[k].max_col; j++){
+                if(img.pixeles[i][j] == lista[k].color){
+                    if(i == 0 || i == img.alto - 1 || j == 0 || j == img.ancho - 1 ||
+                        img.pixeles[i-1][j] != lista[k].color || img.pixeles[i+1][j] != lista[k].color ||
+                        img.pixeles[i][j-1] != lista[k].color || img.pixeles[i][j+1] != lista[k].color ){
+                            borde++;
+                    }
+                }
+            }
+        }
+        lista[k].circu = borde;
+        lista[k].pi = 0.0;
+
+        if(lista[k].diametro > 0)lista[k].pi = (double)lista[k].circu / lista[k].diametro;
+
+        sum_pi += lista[k].pi;
+    }
+
+    //3. Imprimir Áreas
+    double pi_promedio = 0.0;
+    if(M > 0) pi_promedio = sum_pi / M;
+
+    for(int k = 0; k < M; k++){
+        double area = pi_promedio * (lista[k].radio * lista[k].radio);
+        lista[k].area_normalizada = (int)ceil(area);
+        cout<< lista[k].area_normalizada << "u"<<endl;
+    }
+    cout<<endl;
+}
+
+void optimizacion_mates2(int idx, int M, int T, Circulo lista[], int suma_actual, int cant_actual, int oscuridad_actual){
+    if(suma_actual > T)return;
+
+    bool es_mejor = false;
+
+    if(suma_actual > mejor_suma) es_mejor = true;
+
+    if(suma_actual == mejor_suma){
+        if(cant_actual < mejor_cantidad){
+            es_mejor = true;
+        }
+        else if(cant_actual == mejor_cantidad){
+            if(oscuridad_actual < mejor_oscuridad){
+                es_mejor = true;
+            }
+        }
+    }
+
+    if(es_mejor){
+        mejor_cantidad = cant_actual;
+        mejor_suma = suma_actual;
+        mejor_oscuridad = oscuridad_actual;
+
+        for(int i = 0; i < M; i++){
+            mejor_seleccion[i] = seleccion_actual[i];
+        }
+    }
+
+    if(idx == M)return;
+
+    if(suma_actual + lista[idx].area_normalizada <= T){
+        seleccion_actual[idx] = true;
+
+        int nueva_oscuridad = (cant_actual == 0) ? idx : oscuridad_actual;
+
+        optimizacion_mates2(idx + 1, M, T, lista, suma_actual + lista[idx].area_normalizada, cant_actual + 1, nueva_oscuridad);
+
+        seleccion_actual[idx] = false;
+    }
+    optimizacion_mates2(idx + 1, M, T, lista, suma_actual, cant_actual, oscuridad_actual);
 }
 
 int main() {
@@ -123,6 +209,24 @@ int main() {
     Circulo circulos[32];
     analizar_circulos(img, circulos);
 
-    calcular(img, circulos, M, T);
+    calcular(img, circulos, M);
+
+    for(int i = 0; i < 32; i++){
+        seleccion_actual[i] = false;
+        mejor_seleccion[i] = false;
+    }
+
+    optimizacion_mates2(0, M, T, circulos, 0, 0, 9999);
+
+    bool primero = true;
+    for(int i = 0; i < M; i++){
+        if(mejor_seleccion[i] == true){
+            if(!primero) cout << ",";
+            cout<<"Circulo" << (i+1);
+            primero = false;
+        }
+    }
+    cout<<endl;
+
     return 0;
 }
